3bd1ec5dca85f171a924d3401d302cb4af820784
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-06-30 10:33:04 +0200

    feat: implemented simple backEnd featch to display all questions

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,38 @@
+import { useEffect, useState } from "react";
+
+type Question = {
+  id: string,
+  question: string,
+  answer: string,
+}
 
 function App() {
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  const fetchQuestions = async () => {
+
+    try {
+      const response = await fetch("http://localhost:8080/api/questions")
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data: Question[] = await response.json();
+      setQuestions(data);
+      
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchQuestions();
+  }, [])
 
   return (
     <>
+      <ul>
+        {questions.map((question) => <li>Question: {question.question} Answer: {question.answer}</li>)}
+      </ul>
     </>
   )
 }
