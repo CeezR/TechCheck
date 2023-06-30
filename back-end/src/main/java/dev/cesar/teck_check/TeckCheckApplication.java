8c845b6dca44976029786b8e4409aeec122cf2df
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-06-30 08:45:11 +0200

    feat: initialized back-end

---
+++ back-end/src/main/java/dev/cesar/teck_check/TeckCheckApplication.java
@@ -0,0 +1,13 @@
+package dev.cesar.teck_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeckCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeckCheckApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/dev/cesar/teck_check/TeckCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cesar.teck_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeckCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
