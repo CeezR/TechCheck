e605e3a856668f35cacaa0aabbec8a2d3e8a56b2
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-09 18:05:54 +0200

    test: new test to getAllTopic as list of strings

--- back-end/src/main/java/dev/cesar/tech_check/model/TechCheckService.java
+++ back-end/src/main/java/dev/cesar/tech_check/model/TechCheckService.java
@@ -2,6 +2,11 @@ package dev.cesar.tech_check.model;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TechCheckService {
+    public List<String> findAllTopics() {
+        return null;
+    }
 }
--- back-end/src/test/java/dev/cesar/tech_check/model/TechCheckServiceTest.java
+++ back-end/src/test/java/dev/cesar/tech_check/model/TechCheckServiceTest.java
@@ -1,9 +1,13 @@
 package dev.cesar.tech_check.model;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -12,5 +16,13 @@ class TechCheckServiceTest {
 
     @Autowired
     TechCheckService service;
+
+    @Test
+    void testToCheckFindAllTopicsReturnListOfTopicNames() {
+        List<String> topics = service.findAllTopics();
+        assertThat(topics).isNotNull();
+        assertThat(topics).contains("Math");
+        assertThat(topics.size()).isEqualTo(4);
+    }
 
 }
