39b0a5815767ac02f54ca003a59fc5cb71f9f956
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-15 16:58:10 +0200

    fix: removed stricked mode

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -5,7 +5,5 @@ import "bootstrap/dist/css/bootstrap.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
